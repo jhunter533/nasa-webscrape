6b276059802343d70804bb1f3c866d67a6b05610
jhunter533 <jhunter533@gmail.com>
2024-06-07 14:37:20 -0600

    v1 of fetching tle

--- issInfo.py
+++ issInfo.py
@@ -1,2 +1,23 @@
 import requests
-url="https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html"
+import urllib.request
+import urllib.error
+import time
+import sys
+import os
+import json
+url="https://isstracker.spaceflight.esa.int/tledata.txt?ts=1717787338481"
+url2="https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html"
+api="ejRNGboT6MHF36M5JBFN6orgWNa1NPkC376NytQe"
+timestamp=1717787338481
+params={"unique":timestamp}
+def downloadDate(timestamp):
+    response=requests.get(url,params)
+    if response.status_code==200:
+        return response.json()
+
+if __name__=="__main__":
+    #req=urllib.request(url)
+    response=urllib.request.urlopen(url)
+    data=response.read()
+    #data=downloadDate(timestamp)
+    print(data)
